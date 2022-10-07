762b7400acbd19738788164d5f43897de7ed491f
andrewwillette <willette.andrew1846@gmail.com>
2022-10-06 21:54:39 -0500

    master: deleting unnecssary html gens, were straight tmpls baby

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"html/template"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -29,7 +26,6 @@ func list(e ...float64) []float64 {
 }
 
 func main() {
-	initBasicIndexHtml()
 	startServer()
 }
 
@@ -52,69 +48,38 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func renderHomePage(c echo.Context) error {
-	_ = []product{
-		{"images/1.png", "strawberries", "$2.00", 4.0, 251, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-		{"images/2.png", "onions", "$2.80", 5.0, 123, "Morbi sit amet erat vitae purus consequat vehicula nec sit amet purus."},
-		{"images/3.png", "tomatoes", "$3.10", 4.5, 235, "Curabitur tristique odio et nibh auctor, ut sollicitudin justo condimentum."},
-		{"images/4.png", "courgette", "$1.20", 4.0, 251, "Phasellus at leo a purus consequat ornare ac aliquam quam."},
-		{"images/5.png", "broccoli", "$3.80", 3.5, 123, "Maecenas sed ante sagittis, dapibus dui quis, hendrerit orci."},
-		{"images/6.png", "potatoes", "$3.00", 2.5, 235, "Vivamus malesuada est et tellus porta, vel consectetur orci dapibus."},
-	}
-
-	allFiles := []string{"content.tmpl", "footer.tmpl", "header.tmpl", "page.tmpl"}
-
-	var allPaths []string
-	for _, tmpl := range allFiles {
-		allPaths = append(allPaths, "./templates/"+tmpl)
-	}
-
-	templates := template.Must(template.New("").Funcs(template.FuncMap{"subtr": subtr, "list": list}).ParseFiles(allPaths...))
-
-	var processed bytes.Buffer
-	templates.ExecuteTemplate(&processed, "page", templates)
-
-	outputPath := "./static/index.html"
-	f, _ := os.Create(outputPath)
-	w := bufio.NewWriter(f)
-	w.WriteString(string(processed.Bytes()))
-	w.Flush()
-
-	exampleData := map[string]interface{}{
-		"list": list,
-	}
-	err := c.Render(http.StatusOK, "page", exampleData)
+	err := c.Render(http.StatusOK, "page", nil)
 	if err != nil {
-		println(err.Error())
 		return err
 	}
 	return nil
 }
 
-func initBasicIndexHtml() {
-	data := []product{
-		{"images/1.png", "strawberries", "$2.00", 4.0, 251, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
-		{"images/2.png", "onions", "$2.80", 5.0, 123, "Morbi sit amet erat vitae purus consequat vehicula nec sit amet purus."},
-		{"images/3.png", "tomatoes", "$3.10", 4.5, 235, "Curabitur tristique odio et nibh auctor, ut sollicitudin justo condimentum."},
-		{"images/4.png", "courgette", "$1.20", 4.0, 251, "Phasellus at leo a purus consequat ornare ac aliquam quam."},
-		{"images/5.png", "broccoli", "$3.80", 3.5, 123, "Maecenas sed ante sagittis, dapibus dui quis, hendrerit orci."},
-		{"images/6.png", "potatoes", "$3.00", 2.5, 235, "Vivamus malesuada est et tellus porta, vel consectetur orci dapibus."},
-	}
+// func initBasicIndexHtml() {
+// 	data := []product{
+// 		{"images/1.png", "strawberries", "$2.00", 4.0, 251, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+// 		{"images/2.png", "onions", "$2.80", 5.0, 123, "Morbi sit amet erat vitae purus consequat vehicula nec sit amet purus."},
+// 		{"images/3.png", "tomatoes", "$3.10", 4.5, 235, "Curabitur tristique odio et nibh auctor, ut sollicitudin justo condimentum."},
+// 		{"images/4.png", "courgette", "$1.20", 4.0, 251, "Phasellus at leo a purus consequat ornare ac aliquam quam."},
+// 		{"images/5.png", "broccoli", "$3.80", 3.5, 123, "Maecenas sed ante sagittis, dapibus dui quis, hendrerit orci."},
+// 		{"images/6.png", "potatoes", "$3.00", 2.5, 235, "Vivamus malesuada est et tellus porta, vel consectetur orci dapibus."},
+// 	}
 
-	allFiles := []string{"content.tmpl", "footer.tmpl", "header.tmpl", "page.tmpl"}
+// 	allFiles := []string{"content.tmpl", "footer.tmpl", "header.tmpl", "page.tmpl"}
 
-	var allPaths []string
-	for _, tmpl := range allFiles {
-		allPaths = append(allPaths, "./templates/"+tmpl)
-	}
+// 	var allPaths []string
+// 	for _, tmpl := range allFiles {
+// 		allPaths = append(allPaths, "./templates/"+tmpl)
+// 	}
 
-	templates := template.Must(template.New("").Funcs(template.FuncMap{"subtr": subtr, "list": list}).ParseFiles(allPaths...))
+// 	templates := template.Must(template.New("").Funcs(template.FuncMap{"subtr": subtr, "list": list}).ParseFiles(allPaths...))
 
-	var processed bytes.Buffer
-	templates.ExecuteTemplate(&processed, "page", data)
+// 	var processed bytes.Buffer
+// 	templates.ExecuteTemplate(&processed, "page", data)
 
-	outputPath := "./static/index.html"
-	f, _ := os.Create(outputPath)
-	w := bufio.NewWriter(f)
-	w.WriteString(string(processed.Bytes()))
-	w.Flush()
-}
+// 	outputPath := "./static/index.html"
+// 	f, _ := os.Create(outputPath)
+// 	w := bufio.NewWriter(f)
+// 	w.WriteString(string(processed.Bytes()))
+// 	w.Flush()
+// }
